e845b6575133eb7debc0fa5d62e7e1b283d7dd3d
torrhen <toby.coleman96@outlook.com>
2022-07-07 20:01:00 +0100

    Add GetX and GetY tests to main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -4,6 +4,7 @@
 
 #include <iostream>
 #include <cstdint>
+#include <gtest/gtest.h>
 
 namespace tile_world_search
 {
@@ -23,10 +24,33 @@ namespace tile_world_search
 		Tile B(1, 1, Letter::B);
 		Tile C(1, 0, Letter::C);		
 	}
+
+	TEST(TileTest, TestGetX)
+	{
+		Tile t1(1, 2, Letter::P);
+		EXPECT_EQ(t1.getX(), 1);
+		Tile t2(3, 0, Letter::P);
+		EXPECT_EQ(t2.getX(), 3);
+		Tile t3(2, 3, Letter::P);
+		EXPECT_EQ(t3.getX(), 2);
+	}
+
+	TEST(TileTest, TestGetY)
+	{
+		Tile t1(1, 2, Letter::P);
+		EXPECT_EQ(t1.getY(), 2);
+		Tile t2(3, 0, Letter::P);
+		EXPECT_EQ(t2.getY(), 0);
+		Tile t3(2, 3, Letter::P);
+		EXPECT_EQ(t3.getY(), 3);
+	}
 }
 
 int main()
 {
+	::testing::InitGoogleTest();
+	return RUN_ALL_TESTS();
+
 	using namespace tile_world_search;
 
 	std::uint32_t search_type;
